b4bee07041dde53f93d684020e28ebc955dd6a2b
jm-fw <jm@freiwuchs.de>
2022-05-24 19:11:44 +0200

    quickfix

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,33 @@
 <template>
-  <layout>
-    <header class="header">
-      <div class="header-text">
-        <p class="title">Mockup Tool – Overview</p>
-      </div>
-    </header>
-    
-    <div class="bg">
-      <div class="window-wrapper">
-        <div class="">
-          
-        </div>
-      </div>
-    </div>
+  <Layout>
 
-  </layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
 </template>
 
 <script>
 export default {
-  metaInfo() {
-    return {
-      title: 'Mockup Tool – Overview',
-    };
-  },
-  data() {
-    return {};
+  metaInfo: {
+    title: 'Hello, world!'
   }
-};
+}
 </script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
