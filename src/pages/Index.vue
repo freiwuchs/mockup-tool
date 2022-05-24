50ac63e9a0e792d418ca4bf8a82f640bedb47fe1
jm-fw <jm@freiwuchs.de>
2022-05-24 19:02:27 +0200

    quickfix

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,152 @@
 <template>
-  <Layout>
+  <layout>
+    <header class="header">
+      <div class="header-text">
+        <p class="title">Mockup Tool – Overview</p>
+      </div>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+      <nav class="nav" v-if="deviceTypes.length > 1">
+        <span
+          v-for="(deviceType, index) in deviceTypes"
+          :key="`deviceType_${index}`"
+          :class="[
+            `${deviceType} button`,
+            activeDeviceType == deviceType ? '' : 'hollow',
+          ]"
+          @click="clickTypeButton(deviceType)"
+        >
+          {{ deviceType }}
+        </span>
+      </nav>
+    </header>
+    
+    <div class="bg">
+      <div class="window-wrapper">
+        <div class="">
+          
+        </div>
+      </div>
+    </div>
 
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+  </layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
-</script>
+  metaInfo() {
+    return {
+      title: 'Mockup Tool – Overview',
+    };
+  },
+  data() {
+    return {
+      loading: false,
+      activeDeviceType: null,
+      deviceTypes: [],
+      images: {
+        Dektop: [],
+        Tablet: [],
+        Smartphone: [],
+      },
+      currentImage: null,
+    };
+  },
+  computed: {
+    activeDeviceTypeClass() {
+      if (this.activeDeviceType)
+        return `${this.activeDeviceType.toLowerCase()}-view`;
+      return '';
+    },
+    currentImageUrl() {
+      if (this.currentImage)
+        return this.currentImage.file.url;
+      return null;
+    },
+    currentImageName() {
+      if (this.currentImage)
+        return this.currentImage.title;
+      return null;
+    },
+    isPrevImage() {
+      const imageIndex = this.images[this.activeDeviceType].findIndex(img => img == this.currentImage)
+      const prevImage = this.images[this.activeDeviceType][imageIndex - 1];
+      if (prevImage !== undefined) return prevImage;
+      return false;
+    },
+    isNextImage() {
+      const imageIndex = this.images[this.activeDeviceType].findIndex(img => img == this.currentImage)
+      const nextImage = this.images[this.activeDeviceType][imageIndex + 1];
+      if (nextImage !== 'undefined') return nextImage;
+      return false;
+    },
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  methods: {
+    setInitData() {
+      this.images.Desktop = [ ...this.$page.project.desktopMockups ];
+      this.images.Tablet = [ ...this.$page.project.tabletMockups ];
+      this.images.Smartphone = [ ...this.$page.project.smartphoneMockups ];
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+      if (this.images.Desktop.length) this.deviceTypes.push('Desktop');
+      if (this.images.Tablet.length) this.deviceTypes.push('Tablet');
+      if (this.images.Smartphone.length) this.deviceTypes.push('Smartphone');
+
+      this.activeDeviceType = this.getInitDeviceType();
+      this.preloadImage(this.getInitImage());
+    },
+    getInitDeviceType() {
+      if (this.images.Desktop.length) return 'Desktop';
+      if (this.images.Tablet.length) return 'Tablet';
+      if (this.images.Smartphone.length) return 'Smartphone';
+    },
+    getInitImage() {
+      return this.images[this.activeDeviceType][0];
+    },
+    resetCurrentImage() {
+      if (this.activeDeviceType)
+        this.$set(this, 'currentImage', this.images[this.activeDeviceType][0]);
+    },
+    preloadImage(image) {
+      if (process.isClient) {
+        const imageEl = new Image();
+        let timeout;
+        imageEl.onload = () => {
+          this.$set(this, 'currentImage', image);
+          this.loading = false;
+          clearTimeout(timeout);
+        }
+        imageEl.src = image.file.url;
+
+        timeout = setTimeout(() => {
+          this.loading = true;
+        }, 50);
+      }
+    },
+    getPrevImage() {
+      const imageIndex = this.images[this.activeDeviceType].findIndex(img => img == this.currentImage)
+      return this.images[this.activeDeviceType][imageIndex - 1];
+    },
+    getNextImage() {
+      const imageIndex = this.images[this.activeDeviceType].findIndex(img => img == this.currentImage)
+      return this.images[this.activeDeviceType][imageIndex + 1];
+    },
+    clickTypeButton(deviceType) {
+      this.activeDeviceType = deviceType; 
+      this.resetCurrentImage();
+    },
+    clickPrevImage() {
+      if (!this.isPrevImage) return;
+      this.preloadImage(this.getPrevImage());
+    },
+    clickNextImage() {
+      if (!this.isNextImage) return;
+      this.preloadImage(this.getNextImage());
+    },
+  }
+};
+</script>
